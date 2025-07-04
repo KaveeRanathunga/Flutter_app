be9cbab7a1ae11cf7a6a5db53b976cc7e8b1bb95
KaveeRanathunga <156744773+KaveeRanathunga@users.noreply.github.com>
2025-07-04 18:09:23 +0530

    some fetures of app bar

--- lib/main.dart
+++ lib/main.dart
@@ -21,12 +21,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //debugShowCheckedModeBanner: false,
       locale: DevicePreview.locale(context), // Use the locale from DevicePreview 
       //debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       builder: DevicePreview.appBuilder,
       theme: ThemeData(
-         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -36,16 +36,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+ 
   final String title;
 
   @override
@@ -60,11 +51,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -74,21 +60,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-
+ 
 
     return SafeArea(
-      child: Scaffold(
+      
+      child: DefaultTabController(
+        
+        length: 3,
+        child: Scaffold(
         appBar: AppBar(
-      
-      
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
+          //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          
           title: Text("Flutter App"),
           leading: IconButton(
             icon: const Icon(Icons.menu),
@@ -114,48 +96,75 @@ class _MyHomePageState extends State<MyHomePage> {
       
           ],
       
-           flexibleSpace: Image.asset("assets/appbar.jpg", // Use the image from assets
-                                      fit : BoxFit.cover) // Ensure the image covers the app bar
-             
-        ),
-      
-      
-      
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            //
-            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-            // action in the IDE, or press "p" in the console), to see the
-            // wireframe for each widget.
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('You have pushed the button this many times:'),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/appbar.jpg"),
+                fit: BoxFit.cover,
               ),
+            ),
+          ),
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: 'Home'),
+              Tab(icon: Icon(Icons.settings), text: 'Settings'),
+              Tab(icon: Icon(Icons.info), text: 'Info'),
             ],
           ),
+          elevation: 22.0, // Set elevation for the AppBar
+          //backgroundColor: Colors.deepPurple, // Set background color for the AppBar
+          
         ),
-      
-      
-      
+
+        body: TabBarView(
+          children: [
+            // Home Tab Content
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text('You have pushed the button this many times:'),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ],
+              ),
+            ),
+
+
+
+            // Settings Tab Content
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.settings, size: 100, color: Colors.grey),
+                  Text('Settings Page', style: Theme.of(context).textTheme.headlineMedium),
+                ],
+              ),
+            ),
+
+
+            // Info Tab Content
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.info, size: 100, color: Colors.blue),
+                  Text('Info Page', style: Theme.of(context).textTheme.headlineMedium),
+                ],
+              ),
+            ),
+          ],
+        ),
+
         floatingActionButton: FloatingActionButton(
           onPressed: _incrementCounter,
           tooltip: 'Increment',
           child: const Icon(Icons.add),
         ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
       ),
     );
   }
